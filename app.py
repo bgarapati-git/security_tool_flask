bfc9c58ebc8095ff39b7e762c8cdb0acb4152090
bindu-haritha <103258735+bindu-haritha@users.noreply.github.com>
2022-08-10 11:37:03 +0530

    made flask changes for summary page
    made flask changes for summary page

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, render_template
 from flask_cors import CORS, cross_origin
 
 from src.run_security_tool import run_security_tool
@@ -10,28 +10,54 @@ app = Flask(__name__, static_folder='static')
 CORS(app, support_credentials=True, expose_headers=["Content-Disposition"])
 
 
-@cross_origin(supports_credentials=True)
-@app.route("/getFileList", methods=["GET"])
+@app.route("/")
+def render_html():
+    report_list=get_files_list()
+    print(f'type is {type(report_list)}')
+    #report_list=json_obj['report']
+    col_names=["REPORTS","PASS COUNT","FAIL COUNT","VIEW"]
+    keys=["filename","pass_count","fail_count"]
+    context = {
+        "table_title": "Security Assessment Tool",
+        "report_list": report_list,
+        "col_names":col_names,
+        "keys":keys
+    }
+    return render_template("summary.html", title="Security Assessment Tool",**context)
+
+# @cross_origin(supports_credentials=True)
+# @app.route("/getFileList", methods=["GET"])
+# def get_files_list():
+#     project_id = request.args.get('project_id')
+#     # project_id='badri-29apr2022-scrumteam'
+#     status_dict = run_security_tool(project_id, app.root_path)
+#     if status_dict['status'] == "Success":
+#         data = {'reports': status_dict['report_list']}
+#         response = app.response_class(
+#             response=json.dumps(data),
+#             status=200,
+#             mimetype='application/json'
+#         )
+#
+#     else:
+#         response = app.response_class(
+#             response="Error Occurred while creating reports ",
+#             status=404,
+#             mimetype='application/json'
+#         )
+#     return response
+
+
 def get_files_list():
-    project_id = request.args.get('project_id')
-    # project_id='badri-29apr2022-scrumteam'
+    #project_id = request.args.get('project_id')
+    project_id='badri-29apr2022-scrumteam'
     status_dict = run_security_tool(project_id, app.root_path)
     if status_dict['status'] == "Success":
-        data = {'reports': status_dict['report_list']}
-        response = app.response_class(
-            response=json.dumps(data),
-            status=200,
-            mimetype='application/json'
-        )
-
+        #data = {'reports': status_dict['report_list']}
+        data=status_dict['report_list']
     else:
-        response = app.response_class(
-            response="Error Occurred while creating reports ",
-            status=404,
-            mimetype='application/json'
-        )
-    return response
-
+        data=[]
+    return data
 
 @app.route("/getReport", methods=["GET"])
 def get_report_file():
@@ -39,6 +65,8 @@ def get_report_file():
     print(f'filename is {file_name}')
     path = os.path.join(app.root_path, "reports", file_name)
     return send_file(path)
+
+
 
 
 if __name__ == "__main__":

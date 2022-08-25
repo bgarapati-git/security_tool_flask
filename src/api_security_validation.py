f836771e0c4301a4cfa454a2fce9012557183802
LaxmiLahari <99728124+LaxmiLahari@users.noreply.github.com>
2022-08-25 14:26:22 +0530

    added sh file

--- src/api_security_validation.py
+++ src/api_security_validation.py
@@ -1,25 +1,26 @@
 import requests
-import yaml
-
-from constants import service_name, message, rule_id, priority, status_const, api_security, api_rule, compliant, \
+import json
+from constants import service_name, message, rule_id, priority, status_const, api_security, api_rule, url, compliant, \
     high_priority, \
-    pass_status, fail_status, api_unsecured, entity
+    pass_status, fail_status, api_unsecured
 
-
+import yaml 
 def get_urls(filename):
     method=get_urls.__name__
     urls = []
+    methods = []
     try:
         with open(filename) as file: 
              documents = yaml.full_load(file) 
              for app in documents["endpoints"]: 
                 urls += documents["endpoints"][app]["url"] 
+                methods +=documents["endpoints"][app]["method"]
     except Exception as e:
         print(f'Exception occured in {method} method exception is {e}')
     print(urls)
-    return(urls)
+    return(urls,methods)
 
-def check_api(urls): 
+def check_api(urls,methods): 
     method=check_api.__name__
     status_list=[]
     try:
@@ -31,22 +32,22 @@ def check_api(urls):
                         'Authorization': 'Bearer 111111111111111111111',
                         'Content-Type': 'application/json'
                         }
-        for i in urls:
+        for i in range(len(urls)):
             status=[]
-            response1 = requests.request("POST",i)
+            response1 = requests.request(methods[i],urls[i])
             status.append(response1.status_code)
-            response2 = requests.request("POST",i,headers=empty_token_header)
+            response2 = requests.request(methods[i],urls[i],headers=empty_token_header)
             status.append(response2.status_code)
-            response3 = requests.request("POST",i,headers=wrong_token_header)
+            response3 = requests.request(methods[i],urls[i],headers=wrong_token_header)
             status.append(response3.status_code)
             print(status)
             #print(response.status_code)
             value=200
             if value in status:
-                status_list.append({service_name: api_security, rule_id: api_rule, entity: i, priority: high_priority,
+                status_list.append({service_name: api_security, rule_id: api_rule, url: urls[i], priority: high_priority,
                                 status_const:fail_status, message:api_unsecured})
             else:
-                status_list.append({service_name: api_security, rule_id: api_rule, entity: i, priority: high_priority,
+                status_list.append({service_name: api_security, rule_id: api_rule, url: urls[i], priority: high_priority,
                                 status_const:pass_status, message:compliant})
     except Exception as e:
         print(f'Exception occured in {method} method exception is {e}')
---
+++ testframework.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo Enter project ID:
+read projectID
+#echo $projectID
+echo Enter Username:
+read Username
+#echo $Username
+echo "Choose the Services to Test:
+      [1] Cloud Storage
+      [2] BigQuery
+      [3] BigQuery_PII
+      [4] Cloud SQL
+      [5] Service Accounts
+      [6] Cloud Run
+      [7] Cloud Function
+      [8] App Engine
+      [9] API Security
+      [10] Git Validation
+
+      format: [2,4,6]"
+read Services
+#echo $Services
+
+cd security_tool_flask
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python3 app.py badri-29apr2022-scrumteam $projectID
